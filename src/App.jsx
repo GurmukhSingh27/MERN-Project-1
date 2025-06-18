4f075092d0b4521d520f825731f5476615e6bdf6
Gurmukh Singh Dadiala <busygurmukh@gmail.com>
2025-06-18 11:55:35 +0530

    login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Login";
-import Home from "./Home";
 import AppLayout from "./layout/AppLayout";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <AppLayout>
-              <Home />
-            </AppLayout>
-          }
-        />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <AppLayout>
+      <Login />
+    </AppLayout>
   );
 };
 
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,10 +1,80 @@
-import React from "react";
+import { useState } from "react";
 
 const Login = () => {
+  const [formData, setFormData] = useState({ username: "", password: "" });
+
+  const [errors, setErrors] = useState({});
+
+  const [message, setMessage] = useState(null);
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const validate = () => {
+    let newErrors = {};
+    let isValid = true;
+    if (formData.username.length === 0) {
+      newErrors.username = "Username is mandatory";
+      isValid = false;
+    }
+    if (formData.password.length === 0) {
+      newErrors.passworde = "Password is mandatory";
+      isValid = false;
+    }
+
+    setErrors(newErrors);
+    return isValid;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (validate()) {
+      if (formData.username === "admin" && formData.password === "admin") {
+        setMessage("Correct credentials");
+      } else {
+        setMessage("Invalid credentials");
+      }
+    }
+  };
+
   return (
     <>
-      <div className="text-center">
-        <h1>Welcome to Login Page</h1>
+      <div className="container text-center">
+        <h1>Login Page</h1>
+        {message && message}
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Username: </label>
+            <input
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+            />
+            {errors.username && errors.username}
+          </div>
+          <div>
+            <label>Password: </label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+            {errors.password && errors.password}
+          </div>
+          <div>
+            <button>Login</button>
+          </div>
+        </form>
       </div>
     </>
   );
